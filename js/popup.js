8d5b1abd542f9954cb7a19cf1387b71bc40e1a5a
Andrew Warner <wwarner.andrew@gmail.com>
2011-08-01 18:59:57 -0400

    Added performance stats

--- js/popup.js
+++ js/popup.js
@@ -4,4 +4,9 @@ function setValues()
 {
     document.getElementById("domain").innerHTML = localStorage["currentHostname"];
     document.getElementById("updated").innerHTML = dataForHost[0]["visitTime"];
+    document.getElementById("faviconOfCurrentPage").src = localStorage[localStorage["currentHostname"]]
+    document.getElementById("totalTimeToDomLoad").innerHTML = dataForHost[0]["totalTimeToDomLoad"];
+    document.getElementById("totalDNSLookupTime").innerHTML = dataForHost[0]["totalDNSLookupTime"];
+    document.getElementById("totalTimeToFirstResponseByte").innerHTML = dataForHost[0]["totalTimeToFirstResponseByte"];
+    document.getElementById("hostVisits").innerHTML = dataForHost.length;
 }
