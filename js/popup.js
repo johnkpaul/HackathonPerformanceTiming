e10bc5bf899157303688d87c50e7ef9344a3225b
Andrew Warner <wwarner.andrew@gmail.com>
2011-08-01 18:30:06 -0400

    Added visitTime logic

--- js/popup.js
+++ js/popup.js
@@ -3,4 +3,5 @@ window.addEventListener('DOMContentLoaded',setValues, false);
 function setValues()
 {
     document.getElementById("domain").innerHTML = localStorage["currentHostname"];
+    document.getElementById("updated").innerHTML = dataForHost[0]["visitTime"];
 }
