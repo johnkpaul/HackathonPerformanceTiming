b1abd59524698f13b37feb2ee45a8b1adeac6f63
John Paul <john@johnkpaul.com>
2011-08-01 17:12:10 -0400

    add nice performance wrapper and integrate into extension

---
+++ js/nicePerformance/nice.js
@@ -0,0 +1,76 @@
+(function(global){
+
+    if(typeof global.NICE == "undefined"){
+        global.NICE = {}
+    }
+
+    NICE.performance = (function(){
+        function getTotalTimeToDomLoad(){
+            var timing = NICE.getTiming();
+            return timing.domComplete -  timing.fetchStart;
+        }
+
+        function getTotalDNSLookupTime(){
+            var timing = NICE.getTiming();
+            return timing.domainLookupStart -  timing.domainLookupEnd;
+        }
+
+        function getTimeToFirstResponseByte(){
+            var timing = NICE.getTiming();
+            return timing.responseStart -  timing.fetchStart;
+        }
+
+        function toObject(){
+            return {
+                "totalTimeToDomLoad":getTotalTimeToDomLoad(),
+                "totalDNSLookupTime":getTotalDNSLookupTime(),
+                "totalTimeToFirstResponseByte":getTimeToFirstResponseByte()
+            }
+        }
+        return {
+            "getTotalTimeToDomLoad":getTotalTimeToDomLoad,
+            "getTotalDNSLookupTime":getTotalDNSLookupTime,
+            "getTimeToFirstResponseByte":getTimeToFirstResponseByte,
+            "toObject":toObject
+        }
+    })();
+
+    NICE.callbacks = [];
+
+    NICE.addPerformanceCallback = function(callback){
+        if(NICE._loaded){
+            callback(NICE.performance);
+        }else{
+            NICE.callbacks.push(callback);
+        }
+    }
+
+    NICE.setPerformanceObject = function(performance){
+        NICE._performance = performance;
+    }
+
+    NICE.getPerformanceObject = function(){
+        return NICE._performance || global.performance || null;
+    }
+
+    NICE.getTiming = function(){
+        return NICE.getPerformanceObject().timing;
+    }
+
+    NICE.init = function(){
+        function whenLoaded() {
+             NICE._loaded = true;
+             for(var i = 0, len = NICE.callbacks.length;i<len;i++){
+                NICE.callbacks[i](NICE.performance);
+             }
+        }
+        setTimeout(whenLoaded, 6000);
+    }
+
+    NICE.init();
+})(window);
+
+NICE.addPerformanceCallback(function(performance){
+    console.log(performance.toObject());
+})
+
